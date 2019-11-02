9f9e1a7f839d6cf9c80105e1e4c9e52a778b4a29
Rob Jinman <jinmanr@gmail.com>
2019-11-02 15:29:13 +0000

    Renamed a file

---
+++ aws_env_template.sh
@@ -0,0 +1,7 @@
+# Copy and rename to aws_env.sh and set values accordingly.
+
+export CLOUDFRONT_DISTRO_ID=XXXXXXXX
+export VPC_ID=XXXXXXXX
+export SUBNET_1_ID=XXXXXXXX
+export SUBNET_2_ID=XXXXXXXX
+export SUBNET_3_ID=XXXXXXXX
--- aws_ids_template.sh
+++
@@ -1,3 +0,0 @@
-# Copy and rename to aws_ids.sh and set values accordingly.
-
-export CLOUDFRONT_DISTRO_ID=XXXXXXXX
